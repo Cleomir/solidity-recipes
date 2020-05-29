7a9871609e9e416bd80cd0e3d176d3d012031691
Cleomir <cleomir.mg@hotmail.com>
2020-05-29 22:52:20 +0300

    Add tests

--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -66,5 +66,92 @@ contract("Marketplace", ([deployer, seller, buyer]) => {
         from: seller,
       }).should.be.rejected;
     });
+
+    it("list products", async () => {
+      const product = await marketplace.products(productCount);
+
+      assert.equal(
+        product.id.toNumber(),
+        productCount.toNumber(),
+        "id is correct"
+      );
+      assert.equal(product.name, "iPhone X", "name is correct");
+      assert.equal(
+        product.price,
+        web3.utils.toWei("1", "Ether"),
+        "price is correct"
+      );
+      assert.equal(product.owner, seller, "owner is correct");
+      assert.equal(product.purchased, false, "purchased is correct");
+
+      // FAILURE
+      await marketplace.createProduct("", web3.utils.toWei("1", "Ether"), {
+        from: seller,
+      }).should.be.rejected;
+      await marketplace.createProduct("iPhone X", 0, {
+        from: seller,
+      }).should.be.rejected;
+    });
+
+    it("sells products", async () => {
+      // track the seller balance before purchase
+      let oldSellerBalance;
+      oldSellerBalance = await web3.eth.getBalance(seller);
+      oldSellerBalance = new web3.utils.BN(oldSellerBalance);
+
+      // success: buyer makes purchase
+      result = await marketplace.purchaseProduct(productCount, {
+        from: buyer,
+        value: web3.utils.toWei("1", "Ether"),
+      });
+
+      const event = result.logs[0].args;
+      // Check logs
+      assert.equal(
+        event.id.toNumber(),
+        productCount.toNumber(),
+        "id is correct"
+      );
+      assert.equal(event.name, "iPhone X", "name is correct");
+      assert.equal(
+        event.price,
+        web3.utils.toWei("1", "Ether"),
+        "price is correct"
+      );
+      assert.equal(event.owner, buyer, "owner is correct");
+      assert.equal(event.purchased, true, "purchased is correct");
+
+      let newSellerBalance;
+      newSellerBalance = await web3.eth.getBalance(seller);
+      newSellerBalance = new web3.utils.BN(newSellerBalance);
+
+      let price;
+      price = web3.utils.toWei("1", "Ether");
+      price = new web3.utils.BN(price);
+
+      const expectedBalance = oldSellerBalance.add(price);
+      assert.equal(newSellerBalance.toString(), expectedBalance.toString());
+
+      // failure: tries to buy a product that does not exist
+      await marketplace.purchaseProduct(99, {
+        from: buyer,
+        value: web3.utils.toWei("1", "Ether"),
+      }).should.be.rejected;
+
+      await marketplace.purchaseProduct(99, {
+        from: buyer,
+        value: web3.utils.toWei("0.5", "Ether"),
+      }).should.be.rejected;
+
+      await marketplace.purchaseProduct(productCount, {
+        from: deployer,
+        value: web3.utils.toWei("1", "Ether"),
+      }).should.be.rejected;
+
+      await marketplace.purchaseProduct(productCount, {
+        from: buyer,
+        value: web3.utils.toWei("1", "Ether"),
+      }).should.be.rejected;
+    });
   });
 });
