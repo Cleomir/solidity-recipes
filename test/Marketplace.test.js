7f560c193417939d5ee803b285374d339a5c5c3c
Cleomir <cleomir.mg@hotmail.com>
2020-05-29 15:13:08 +0300

    Add tests

--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-undef */
 const Marketplace = artifacts.require("./Marketplace.sol");
 
-contract("Marketplace", (accounts) => {
+require("chai").use(require("chai-as-promised")).should();
+
+contract("Marketplace", ([deployer, seller, buyer]) => {
   let marketplace;
 
   before(async () => {
@@ -20,6 +22,49 @@ contract("Marketplace", (accounts) => {
     it("has a name", async () => {
       const name = await marketplace.name();
       assert.equal(name, "Dapp University Marketplace");
+    });
+  });
+
+  describe("products", async () => {
+    let result;
+    let productCount;
+
+    before(async () => {
+      result = await marketplace.createProduct(
+        "iPhone X",
+        web3.utils.toWei("1", "Ether"),
+        { from: seller }
+      );
+      productCount = await marketplace.productCount();
+    });
+
+    it("creates products", async () => {
+      // SUCCESS
+      assert.equal(productCount, 1);
+
+      const event = result.logs[0].args;
+
+      assert.equal(
+        event.id.toNumber(),
+        productCount.toNumber(),
+        "id is correct"
+      );
+      assert.equal(event.name, "iPhone X", "name is correct");
+      assert.equal(
+        event.price,
+        web3.utils.toWei("1", "Ether"),
+        "price is correct"
+      );
+      assert.equal(event.owner, seller, "owner is correct");
+      assert.equal(event.purchased, false, "purchased is correct");
+
+      // FAILURE
+      await marketplace.createProduct("", web3.utils.toWei("1", "Ether"), {
+        from: seller,
+      }).should.be.rejected;
+      await marketplace.createProduct("iPhone X", 0, {
+        from: seller,
+      }).should.be.rejected;
     });
   });
 });
