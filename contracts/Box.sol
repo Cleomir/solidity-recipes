4650a79a54d0fd27ccfb4da01ce99daa171754dd
Cleomir <cleomir.mg@hotmail.com>
2020-05-30 23:54:11 +0300

    Add contracts

---
+++ contracts/Box.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Box is Ownable {
+    uint256 private value;
+
+    event ValueChanged(uint256 newValue);
+
+    function store(uint256 newValue) public onlyOwner {
+        value = newValue;
+        emit ValueChanged(newValue);
+    }
+
+    function increment() public {
+        value = value++;
+        emit ValueChanged(value);
+    }
+
+    function retrieve() public view returns (uint256) {
+        return value;
+    }
+}
